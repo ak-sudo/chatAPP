0234e6eb9811c77b4b2b867386a31316cb0a338c
Akshat Kala <72704561+ak-sudo@users.noreply.github.com>
2025-06-27 14:26:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const io = require('socket.io')(8080, {
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
     cors: {
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        origin: 'https://chatapp-lrdx.onrender.com/' || 'http://localhost:5173',
     }
 });
 
@@ -17,7 +19,6 @@ const Conversations = require('./db/models/conversation');
 const Messages = require('./db/models/messages');
 
 // app Use
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -223,6 +224,6 @@ app.get('/api/users/:userId', async (req, res) => {
     }
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('listening on port ' + port);
 })
